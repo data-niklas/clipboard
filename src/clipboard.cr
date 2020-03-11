6d99ed3f95c1d96b7beb13cdfd04105eb0a61723
Niklas Loeser <niklas@4loeser.net>
2020-03-11 20:23:48 +0100

    renamed enums to fit the default coding style

--- src/clipboard.cr
+++ src/clipboard.cr
@@ -1,22 +1,22 @@
 module Clipboard
     @[Link(ldflags: "-L/lib/ -I/lib/ -lclipboard -lxcb")]
     lib LibClip
-        type Clipboard_C = Void*
-        type Clipboard_Opts = Void*
-        enum Clipboard_Mode
+        type ClipboardC = Void*
+        type ClipboardOpts = Void*
+        enum ClipboardMode
             LCB_CLIPBOARD
             LCB_PRIMARY
             LCB_SECONDARY
             LCB_MODE_END
         end
-        fun set_text = clipboard_set_text(cb : Clipboard_C*, text : LibC::Char*) : Bool
-        fun set_text_ex = clipboard_set_text_ex(cb : Clipboard_C*, text : LibC::Char*, length : LibC::Int, mode : Clipboard_Mode) : Bool
-        fun get_text = clipboard_text(cb : Clipboard_C*) : LibC::Char*
-        fun get_text_ex = clipboard_text_ex(cb : Clipboard_C*, length : LibC::Int*, mode : Clipboard_Mode) : LibC::Char*
-        fun has_ownership? = clipboard_has_ownership(cb : Clipboard_C*, mode : Clipboard_Mode) : Bool
-        fun clear = clipboard_clear(cb : Clipboard_C*, mode : Clipboard_Mode) : Void
-        fun free = clipboard_free(cb : Clipboard_C*) : Void
-        fun new = clipboard_new(opts : Clipboard_Opts*) : Clipboard_C*
+        fun set_text = clipboard_set_text(cb : ClipboardC*, text : LibC::Char*) : Bool
+        fun set_text_ex = clipboard_set_text_ex(cb : ClipboardC*, text : LibC::Char*, length : LibC::Int, mode : ClipboardMode) : Bool
+        fun get_text = clipboard_text(cb : ClipboardC*) : LibC::Char*
+        fun get_text_ex = clipboard_text_ex(cb : ClipboardC*, length : LibC::Int*, mode : ClipboardMode) : LibC::Char*
+        fun has_ownership? = clipboard_has_ownership(cb : ClipboardC*, mode : ClipboardMode) : Bool
+        fun clear = clipboard_clear(cb : ClipboardC*, mode : ClipboardMode) : Void
+        fun free = clipboard_free(cb : ClipboardC*) : Void
+        fun new = clipboard_new(opts : ClipboardOpts*) : ClipboardC*
     end
 
     def self.get_text()
@@ -36,7 +36,7 @@ module Clipboard
 
     def self.clear()
         clip = LibClip.new(nil)
-        LibClip.clear clip, LibClip::Clipboard_Mode::LCB_CLIPBOARD
+        LibClip.clear clip, LibClip::ClipboardMode::LCB_CLIPBOARD
         LibClip.free clip
     end
 
